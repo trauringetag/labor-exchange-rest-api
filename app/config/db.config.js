ef995b25ebf59c0c85569f07ca3f1bfbf9641523
Kidd Kaine <humans.vlone@gmail.com>
2023-02-12 14:36:34 +0500

    0.02 Добавление конфигураций для СУБД

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "osp74.beget.tech",
+    USER: "osp74_klimkin",
+    PASSWORD: "Q123q123!",
+    DB: "osp74_klimkin",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
