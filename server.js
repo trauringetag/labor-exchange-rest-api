f821eaa3541de0aca5cd7cee98027491f2113e37
Kidd Kaine <humans.vlone@gmail.com>
2023-02-12 14:27:47 +0500

    0.01 Написание главного файла сервера

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const corsOptions = { origin: 'http://localhost:8081' };
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// const db = require('./app/models');
+//
+// db.sequelize.sync().then(() => {
+//     console.log('Синхронизированная база данных');
+// }).catch(err => {
+//     console.log(`Не удалось синхронизировать базу данных: ${err.message}`);
+// });
+
+app.get('/', (req, res) => {
+    res.json({ response: "REST API для веб-сервиса биржи труда" });
+});
+
+// require('./app/routes/tutorial.routes')(app);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Сервер работает по адресу: http://localhost:${PORT}`);
+});
